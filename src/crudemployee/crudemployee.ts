a9b349962236f52e9bdb5639b79d7ec1f68319e0
DouglasEPS <douglaseduardo2206@gmail.com>
2026-08-18 18:12:43 -0300

    fix: apagando pastas desnecessarias

--- src/crudemployee/crudemployee.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-crudemployee',
-  imports: [],
-  templateUrl: './crudemployee.html',
-  styleUrl: './crudemployee.css',
-})
-export class CRUDemployee {}
