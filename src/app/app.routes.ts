eee6540ed65f8c29dab03a272fc589e8dcbf8593
Antonio Amorim <amorimnetoo02@gmail.com>
2026-08-19 20:05:30 -0300

    fix: adiciona rota de login no app.routes (RF002)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { UserAutoRegister } from './user-auto-register/user-auto-register';
+import { Login } from './login/login';
 
 export const routes: Routes = [
     {
         path: 'user-auto-register',
         component: UserAutoRegister,
+    },
+    {
+        path: 'login',
+        component: Login,
     }
 ];
