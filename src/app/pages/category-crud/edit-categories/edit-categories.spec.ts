5d43afec885844d072ac85845f0747f395717316
Eduferruzzi <eduferruzzi@gmail.com>
2026-09-10 21:22:21 -0300

    feat: rf017 crud categoria de equipamentos

---
+++ src/app/pages/category-crud/edit-categories/edit-categories.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EditCategories } from './edit-categories';
+
+describe('EditCategories', () => {
+  let component: EditCategories;
+  let fixture: ComponentFixture<EditCategories>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [EditCategories],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(EditCategories);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
