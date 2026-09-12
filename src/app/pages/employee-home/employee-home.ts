ed483d4f935c15fc4cafad4dbeefbe65e438c97f
DouglasEPS <douglaseduardo2206@gmail.com>
2026-09-12 12:35:22 -0300

    Refactor RF012 - Utilizando dados mockados e implementando filtros + funcoes do servicos de employee

--- src/app/pages/employee-home/employee-home.ts
+++ src/app/pages/employee-home/employee-home.ts
@@ -1,27 +1,25 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { Solicitacao } from '../../models/Solicitacao';
 import { SolicitacaoService } from '../../services';
 
-
 @Component({
   selector: 'app-employee-home',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './employee-home.html',
   styleUrl: './employee-home.css',
 })
-export class EmployeeHome {
-  
-  solicitacoes : Solicitacao[] = [];
-  constructor (private solicitacaoService : SolicitacaoService){}
-  
-  ngOnInit() : void{
+export class EmployeeHome implements OnInit {
+  solicitacoes: Solicitacao[] = [];
+
+  constructor(private solicitacaoService: SolicitacaoService) {}
+
+  ngOnInit(): void {
     this.solicitacoes = this.solicitacaoService.listarAbertas();
   }
 
-  verif(text : string, limite : number = 30) : string {
-    return text.length > limite ? text.substring(0, limite) + "..." : text;
+  verif(text: string, limite: number = 30): string {
+    return text.length > limite ? text.substring(0, limite) + '...' : text;
   }
-
-  
 }
